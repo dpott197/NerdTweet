b12bcf0008b0ac2be56e0350ff3981e7660613a1
Darren Pottinger <darren.pottinger@gmail.com>
2017-03-18 15:27:26 -0400

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/nerdtweet/web/AuthorizationInterceptor.java
@@ -0,0 +1,35 @@
+package com.bignerdranch.android.nerdtweet.web;
+
+import android.util.Log;
+
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class AuthorizationInterceptor implements Interceptor {
+
+    private static final String TAG = "AuthInterceptor";
+    private static final String AUTH_HEADER = "Authorization";
+
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+        Request request = chain.request();
+        TwitterOauthHelper oauthHelper = TwitterOauthHelper.get();
+
+        try {
+            String authHeaderString =
+                    oauthHelper.getAuthorizationHeaderString(request);
+            request = request.newBuilder()
+                    .addHeader(AUTH_HEADER, authHeaderString)
+                    .build();
+        } catch (InvalidKeyException | NoSuchAlgorithmException e) {
+            Log.e(TAG, "Failed to get auth header string", e);
+        }
+
+        return chain.proceed(request);
+    }
+}
